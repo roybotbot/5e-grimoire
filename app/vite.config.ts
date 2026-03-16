f78b76c69a8be196d4f562be9c931082c15df283
Roy Natian <roy@natian.io>
2026-03-16 19:44:25 -0400

    fix: set base path to / for custom domain

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/5etools-src/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   build: {
     outDir: "../docs",
